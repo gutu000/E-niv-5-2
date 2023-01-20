073b516c751410a7c0fbf6b3c33bc5f56e497ac4
Pingvu <nhlpool27@gmail.com>
2023-01-20 14:22:37 +0100

    Add project files.

---
+++ E-nivå 5.2/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] tal = new int[3];
+
+tal[0] = 5;
+tal[1] = 10;
+
+{
+    Console.WriteLine("Första tal "+ tal[0]);
+    Console.WriteLine("Andra tal "+ tal[1]);
+    Console.WriteLine("Skriv in det tredje talet");
+}
+tal[2] = int.Parse(Console.ReadLine());
+{
+    Console.WriteLine($"Alla talen i programmet {tal[0]}, {tal[1]}, {tal[2]}");
+}
+
